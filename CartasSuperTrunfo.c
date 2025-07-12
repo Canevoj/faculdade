24f0832e244745d53f8e8765f8ea87f2492b129d
Michael Lucas Pereira Jovenaci <lucasjovenaci@gmail.com>
2025-07-12 22:51:40 +0000

    app pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,45 +14,47 @@ int main()
 
     printf("Você vai precisar cadastrar dois países para jogar.\n");
 
-    printf("Vamos começar com o primeiro país.\n");
+    printf("--- PRIMEIRO PAÍS ---\n");
 
     printf("Qual o nome do primeiro país?\n");
     scanf("%s", nome1);
 
-    printf("Quantos habitantes existem no(a) %s?\n", nome1);
+    printf("Quantos habitantes existem no(a) %s? (digite o número completo, sem pontos ou vírgulas)\n", nome1);
     scanf("%d", &povo1);
 
-    printf("Qual a área do(a) %s em km²?\n", nome1);
+    printf("Qual a área do(a) %s em km²? (use ponto para decimais, se houver)\n", nome1);
     scanf("%f", &area1);
 
-    printf("Qual o PIB do(a) %s em bilhões de dólares?\n", nome1);
+    printf("Qual o PIB do(a) %s em bilhões de dólares? (use ponto para decimais)\n", nome1);
     scanf("%f", &pib1);
 
-    printf("Quantos pontos turísticos tem no(a) %s?\n", nome1);
+    printf("Quantos pontos turísticos tem no(a) %s? (reconhecidos pela UNESCO)\n", nome1);
     scanf("%d", &pontos1);
 
     printf("Muito bem, agora vamos cadastrar o segundo país.\n");
+    
+    printf("--- SEGUNDO PAÍS ---\n");
 
     printf("Qual o nome do segundo país?\n");
     scanf("%s", nome2);
 
-    printf("Quantos habitantes existem no(a) %s?\n", nome2);
+    printf("Quantos habitantes existem no(a) %s?  (digite o número completo, sem pontos ou vírgulas)\n", nome2);
     scanf("%d", &povo2);
 
-    printf("Qual a área do(a) %s em km²?\n", nome2);
+    printf("Qual a área do(a) %s em km²? (use ponto para decimais, se houver)\n", nome2);
     scanf("%f", &area2);
 
-    printf("Qual o PIB do(a) %s em bilhões de dólares?\n", nome2);
+    printf("Qual o PIB do(a) %s em bilhões de dólares? (use ponto para decimais)\n", nome2);
     scanf("%f", &pib2);
 
-    printf("Quantos pontos turísticos tem no(a) %s?\n", nome2);
+    printf("Quantos pontos turísticos tem no(a) %s? (reconhecidos pela UNESCO)\n", nome2);
     scanf("%d", &pontos2);
 
-    printf("Perfeito! Os países foram cadastrados com sucesso!\n");
+    printf("\nPerfeito! Os países foram cadastrados com sucesso!\n");
     printf("Vamos conferir os países cadastrados:\n");
 
-    printf("\n→País 1: %s\n•População: %d\n•Área: %f km²\n•PIB:$ %3f bilhões\n•Quantidade de pontos turísticos: %d\n", nome1, povo1, area1, pib1, pontos1);
-    printf("\n→País 2: %s\n•População: %d\n•Área: %f km²\n•PIB:$ %3f bilhões\n•Quantidade de pontos turísticos: %d\n", nome2, povo2, area2, pib2, pontos2);
+    printf("\n→ País 1: %s\n• População: %'d habitantes\n• Área: %'.0f km²\n• PIB: US$ %.2f bilhões\n• Quantidade de pontos turísticos: %d\n", nome1, povo1, area1, pib1, pontos1);
+    printf("\n→ País 2: %s\n• População: %'d habitantes\n• Área: %'.0f km²\n• PIB: US$ %.2f bilhões\n• Quantidade de pontos turísticos: %d\n", nome2, povo2, area2, pib2, pontos2);
 
     return 0;
 }
